a505961149f8eb482a471c910174ee935b51b39a
Tarun Kumar Jain <microbiteindia@gmail.com>
2026-07-29 17:55:39 +0530

    Created Express server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "MarketBridge Scraper is running."
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
