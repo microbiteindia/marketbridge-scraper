470a32c09a4bebac1fe4b310882a5923ea46c735
Tarun Kumar Jain <microbiteindia@gmail.com>
2026-08-24 00:52:13 +0530

    Added Browser Manager

--- src/server.js
+++ src/server.js
@@ -1,16 +1,43 @@
 const express = require("express");
+const { getBrowser } = require("./browser/browser");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+
     res.json({
         success: true,
         message: "MarketBridge Scraper is running."
     });
+
+});
+
+app.get("/browser", async (req, res) => {
+
+    try {
+
+        await getBrowser();
+
+        res.json({
+            success: true,
+            message: "Browser started successfully."
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+
 });
 
 app.listen(PORT, () => {
+
     console.log(`Server running on port ${PORT}`);
+
 });
