f6043f850f48d93c0a8855187679084255f660d6
Tarun Kumar Jain <microbiteindia@gmail.com>
2026-08-28 19:16:19 +0530

    Fix Puppeteer Chrome version

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+const express = require("express");
+
 const amazonRoute = require("./routes/amazon");
 const flipkartRoutes = require("./routes/flipkart");
 
-const express = require("express");
+const searchRoute = require("./routes/search");
+const amazonSearchRoute = require("./routes/search-amazon");
+const flipkartSearchRoute = require("./routes/search-flipkart");
+
 const { getBrowser } = require("./browser/browser");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
 
@@ -41,6 +49,14 @@ app.get("/browser", async (req, res) => {
 
 app.use("/amazon", amazonRoute);
 app.use("/flipkart", flipkartRoutes);
+
+app.use("/search", searchRoute);
+
+// /search-amazon?q=iphone+15
+app.use("/search-amazon", amazonSearchRoute);
+
+// /search-flipkart?q=iphone+15
+app.use("/search-flipkart", flipkartSearchRoute);
 
 app.listen(PORT, () => {
 
